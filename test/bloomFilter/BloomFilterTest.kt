ebf4ba9d541428aded14c5786dcc093084eb8098
GinormousSalmon <terleczcky.alex@gmail.com>
2020-12-01 06:45:37 +0300

    some tests added

--- test/bloomFilter/BloomFilterTest.kt
+++ test/bloomFilter/BloomFilterTest.kt
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Assertions.*
 import java.lang.IllegalStateException
 import kotlin.math.abs
 import java.util.*
+import kotlin.math.E
+import kotlin.math.pow
 
 
 class BloomFilterTest {
 
-    private val size = 50
+    private var size = 50
+    private val k = 3  // number of hash functions
 
     private fun intHash1(a: Int): Int = abs(a) % size
     private fun intHash2(a: Int): Int = (abs(a) / size) % size
@@ -39,7 +42,7 @@ class BloomFilterTest {
             assertTrue(bloom.mightContains(e))
     }
 
-    private fun getRandomString(length: Int) : String {
+    private fun getRandomString(length: Int): String {
         val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
         return (1..length)
             .map { allowedChars.random() }
@@ -62,6 +65,35 @@ class BloomFilterTest {
                 for (j in 0..i)
                     assertTrue(bloom.mightContains(controlList[i]))
             }
+        }
+    }
+
+    @Test
+    fun probabilityTest() {
+        val n = 3000
+        size = 5000
+        val controlList = mutableListOf<String>()
+        val random = Random()
+        for (i in 0 until n)
+            controlList.add(getRandomString(random.nextInt(100)))
+        val bloom = BloomFilter<String>(size)
+        bloom.setHashes(::stringHash1, ::stringHash2, ::stringHash3)
+        for (e in controlList)
+            assertFalse(bloom.mightContains(e))
+        for (i in controlList.indices) {
+            var falsePositives = 0
+            bloom.add(controlList[i])
+            for (j in 0..i)
+                assertTrue(bloom.mightContains(controlList[i]))
+            val pos0 = (1 - E.pow(-k * (i + 1) / size.toDouble())).pow(k)
+            for (j in i + 1 until controlList.size)
+                if (bloom.mightContains(controlList[j]))
+                    falsePositives += 1
+            var pos = falsePositives / n.toDouble()
+            println("$pos0 $pos $falsePositives")
+            if (pos < 0.1)  // the formula doesn't work on very small possibility values
+                pos = 0.0
+            assertTrue(pos * 0.95 < pos0)   //accounting for error
         }
     }
 
